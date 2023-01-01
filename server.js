ae4eb438204334dc39685d00f4b60a46a79137f2
kmaddox2122 <kmaddox2122@gmail.com>
2023-01-01 17:54:49 -0600

    server up and running, connected to insomnia

--- server.js
+++ server.js
@@ -21,3 +21,14 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the movies_db database.`)
 );
+
+//write get/post/delete routes here
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
